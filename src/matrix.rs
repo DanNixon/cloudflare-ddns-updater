5866bb036dfef37d07ff5b14e792f304ea62d108
Dan Nixon <dan@dan-nixon.com>
2022-06-13 08:20:58 +0100

    Use Matrix SDK 0.5.0

--- src/matrix.rs
+++ src/matrix.rs
@@ -1,27 +1,27 @@
 use anyhow::{anyhow, Result};
 use matrix_sdk::{
-    ruma::{
-        events::{room::message::MessageEventContent, AnyMessageEventContent},
-        RoomId, UserId,
-    },
-    Client, SyncSettings,
+    config::SyncSettings,
+    ruma::{events::room::message::RoomMessageEventContent, RoomId, UserId},
+    Client,
 };
 use serde::Deserialize;
-use std::convert::TryFrom;
 
 #[derive(Deserialize, Debug, Clone)]
 pub(crate) struct MatrixConfig {
     username: String,
     password: String,
     room_id: String,
-    pub verbose: bool,
+    pub(crate) verbose: bool,
 }
 
 pub(crate) async fn login(config: &MatrixConfig) -> Result<Client> {
     log::debug!("Logging in to Matrix...");
 
-    let user = UserId::try_from(config.username.clone())?;
-    let client = Client::new_from_user_id(user.clone()).await?;
+    let user = UserId::parse(config.username.clone())?;
+    let client = Client::builder()
+        .homeserver_url(format!("https://{}", user.server_name()))
+        .build()
+        .await?;
     client
         .login(user.localpart(), &config.password, None, None)
         .await?;
@@ -33,16 +33,13 @@ pub(crate) async fn login(config: &MatrixConfig) -> Result<Client> {
 }
 
 pub(crate) async fn send_message(config: &MatrixConfig, client: &Client, msg: &str) -> Result<()> {
-    let room = RoomId::try_from(config.room_id.clone())?;
+    let room = RoomId::parse(config.room_id.clone())?;
 
     log::debug!("Sending message...");
     client
         .get_joined_room(&room)
         .unwrap()
-        .send(
-            AnyMessageEventContent::RoomMessage(MessageEventContent::text_plain(msg)),
-            None,
-        )
+        .send(RoomMessageEventContent::text_plain(msg), None)
         .await?;
 
     let settings = SyncSettings::default().token(
